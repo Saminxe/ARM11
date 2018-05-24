cd71761ad4173549f88c3784a3913949a9045df9
Chang Shen Ho <chang.ho17@imperial.ac.uk>
2018-05-24 14:55:42 +0100

    Fixed the registers/memory

--- src/emulate.c
+++ src/emulate.c
@@ -1,10 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
-#define FLAGS 16
+#define SP 13 //shared pointer
+#define LR 14 //link register
+#define PC 15 //program counter
+#define CPSR 16 //flags register
+
+//void loadInto(char *memory, )
 
 int main(int argc, char **argv) {
-  memory = calloc(65536, sizeof(char));
-  registers = calloc(17, sizeof(long));
+  int *memory;
+  int *registers;
+  //FILE *proc;
+
+  if (argc != 2) {
+    printf("Usage = %s filename\n", argv[0]);
+    return EXIT_FAILURE;
+  }
+
+  memory = (int *) calloc(65536, sizeof(char));
+  registers = (int *) calloc(17, sizeof(long));
+  //proc = fopen(argv[1], "r");
+
+  free(memory);
+  free(registers);
 
   return EXIT_SUCCESS;
 }
