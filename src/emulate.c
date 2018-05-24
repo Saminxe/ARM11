8fa4fdcf5d259d61b6670aeeaf1396f899925cae
Chang Shen Ho <chang.ho17@imperial.ac.uk>
2018-05-24 14:22:56 +0100

    Added Registers

--- src/emulate.c
+++ src/emulate.c
@@ -1,5 +1,10 @@
 #include <stdlib.h>
+#include <stdio.h>
+#define FLAGS 16
 
 int main(int argc, char **argv) {
+  memory = calloc(65536, sizeof(char));
+  registers = calloc(17, sizeof(long));
+
   return EXIT_SUCCESS;
 }
