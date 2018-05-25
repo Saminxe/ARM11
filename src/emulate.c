748a1fb7ed28bb877f2fd5f653fdb675ee78a4d0
haru <vulgal@gmail.com>
2018-05-25 18:28:10 +0800

    Added skeletons for processing instructions

--- src/emulate.c
+++ src/emulate.c
@@ -9,22 +9,62 @@
 void printMemoryComposition(unsigned char *memory, int size)
 {
     for (int i = 0; i < size; i++)
-      printf("Memory at %d = %2x\n", i, memory[i]);
+      printf("Memory at %d = %02x\n", i, memory[i]);
 }
 
 void printRegisterComposition(unsigned long *registers)
 {
   for (int i = 0; i < 13; i++)
-    printf("E%d: %lx\n", i, registers[i]);
-  printf("ESP: %lx\n", registers[SP]);
-  printf("ELR: %lx\n", registers[LR]);
-  printf("EPC: %lx\n", registers[PC]);
-  printf("CPSR: %lx\n", registers[CPSR]);
+    printf("E%02d: %08lx\n", i, registers[i]);
+  printf("ESP: %08lx\n", registers[SP]);
+  printf("ELR: %08lx\n", registers[LR]);
+  printf("EPC: %08lx\n", registers[PC]);
+  printf("CPSR: %08lx\n", registers[CPSR]);
 }
 
+/*** End of debugging tools ***/
 
+/*** Processing Instructions ***/
+/* All data processing instructions take the base address of the
+  memory and registers, and the instruction as arguments */
+void dataProcess(unsigned char *memory, unsigned long *registers, unsigned long instr)
+{
+  printf("This is a data processing instruction\n");
+}
 
-/* End of debugging tools */
+void multiply(unsigned char *memory, unsigned long *registers, unsigned long instr)
+{
+  printf("This is a multiply instruction\n");
+}
+
+void singleDataTransfer(unsigned char *memory, unsigned long *registers, unsigned long instr)
+{
+  printf("This is an SDT instruction\n");
+}
+
+void branchDataTransfer(unsigned char *memory, unsigned long *registers, unsigned long instr)
+{
+  printf("This is a branch instruction\n");
+}
+
+/*** Pipeline ***/
+void fetch(unsigned char *memory, unsigned long *registers)
+{
+  unsigned long pc = *(registers + PC);
+  unsigned long instr = memory[pc + 3] |
+    memory[pc + 2] << 8 |
+    memory[pc + 1] << 16 |
+    memory[pc] << 24;
+  if (~(instr ^ 0x90) & 0x0FC000F0)
+    multiply(memory, registers, instr);
+  else if (~(instr ^ 0x0) & 0x0C000000)
+    dataProcess(memory, registers, instr);
+  else if (~(instr ^ 0x04000000) & 0x0C600000)
+    singleDataTransfer(memory, registers, instr);
+  else if (~(instr ^ 0x0A000000) & 0x0F000000)
+    branchDataTransfer(memory, registers, instr);
+  else printf("not a valid instruction u schmuck");
+}
 
 int main(int argc, char **argv)
 {
@@ -52,8 +92,10 @@ int main(int argc, char **argv)
   fseek(proc, 0, SEEK_SET);
   fread(memory, sizeof(char), procSize, proc);
 
-  //printMemoryComposition(memory, procSize);
-  //printRegisterComposition(registers);
+  fetch(memory, registers);
+
+  printMemoryComposition(memory, procSize);
+  printRegisterComposition(registers);
 
   free(memory);
   free(registers);
