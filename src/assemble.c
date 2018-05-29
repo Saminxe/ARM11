410991c540c0a764181a6ed7986548218542749a
Chang Shen Ho <chang.ho17@imperial.ac.uk>
2018-05-29 14:41:32 +0100

    Added a map struct

--- src/assemble.c
+++ src/assemble.c
@@ -1,13 +1,21 @@
 #include "assemble.h"
 
+int contains(char *string, char c)
+{
+  int length = strlen(string);
+  for (int i = 0; i < length; i++) {
+    if (string[i] == c) return 1;
+  }
+  return 0;
+}
+
 int main(int argc, char **argv) {
   FILE* src;
   char *buffer = calloc(BUFFER_SIZE, sizeof(char));
-  char **tokens;
 
   /* Tests for correct amount of input variables */
-  if (argc != 2) {
-    fprintf(stderr, "Usage = %s filename\n", argv[0]);
+  if (argc != 3) {
+    fprintf(stderr, "Usage = %s input output\n", argv[0]);
     return EXIT_FAILURE;
   }
 
@@ -20,14 +28,42 @@ int main(int argc, char **argv) {
     return EXIT_FAILURE;
   }
 
-  /* Tokenization Loop */
+  /* Symbol Tablulation Loop */
   while (fgets(buffer, BUFFER_SIZE, src) != NULL) {
-    
+    char *nextToken = strtok(buffer, " ,");
+    while (nextToken) {
+      if (contains(nextToken, ':')) {
+        printf("LABEL: ");
+      }
+      printf("%s\n", nextToken);
+      nextToken = strtok(NULL, " ,");
+    }
   }
 
+  /* Translation Loop */
+  while (fgets(buffer, BUFFER_SIZE, src) != NULL) {
 
+  }
 
   fclose(src);
   free(buffer);
 
+}
+
+/*** Lookup Functions ***/
+int mapContainsKey(Map m, char *k)
+{
+  for (int i = 0; i < m.size; i++) {
+    if (m.entries[i].key == k)
+      return 1;
+  }
+  return 0;
+}
+
+uint8_t getKeyVal(Map m, char *k) {
+  for (int i = 0; i < m.size; i++) {
+    if (m.entries[i].key == k)
+      return m.entries[i].val;
+  }
+  return -1;
 }
--- src/assemble.h
+++ src/assemble.h
@@ -1,5 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
 
-#define BUFFER_SIZE 2048
+#define BUFFER_SIZE 512
+
+typedef struct {
+  char *key;
+  uint8_t val;
+} Entry;
+
+typedef struct {
+  int size;
+  Entry entries[];
+} Map;
+
+int contains(char *string, char c);
+int mapContainsKey(Map m, char *k);
+uint8_t getKeyVal(Map m, char *k);
