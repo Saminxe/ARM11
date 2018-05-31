2a4984bacc3278042ac8852453d0dcbf05b1a5c7
samuel <st5317@ic.ac.uk>
2018-05-31 18:36:04 +0100

    Progress made on the second pass

--- src/assemble.c
+++ src/assemble.c
@@ -9,8 +9,7 @@ void printSymtab(SymbolTable s) {
   } while (i < s.size);
 }
 
-int contains(char *string, char c)
-{
+int contains(char *string, char c) {
   int length = strlen(string);
   for (int i = 0; i < length; i++) {
     if (string[i] == c) return 1;
@@ -23,9 +22,8 @@ int optab(char *string) {
   return 1;
 }
 
-int main(int argc, char **argv)
-{
-  FILE* src;
+int main(int argc, char **argv) {
+  FILE *src;
   char *buffer = calloc(BUFFER_SIZE, sizeof(char));
   uint8_t locctr = 0; // Starting address for memory allocations
   SymbolTable symtab = {0, calloc(DEFAULT_MAP_SIZE, sizeof(Symbol))};
@@ -74,13 +72,42 @@ int main(int argc, char **argv)
 
   printSymtab(symtab);
 
+  uint32_t *bmem = calloc(16384 + sizeof(uint32_t));
+  int *pmem = 0;
+
   /* Translation Loop */
   while (fgets(buffer, BUFFER_SIZE, src) != NULL) {
+    uint32_t instr;
     char *opcode = strtok(buffer, " ");
     char *operands = strtok(NULL, " ");
-    if(*opcode != ';') {
-      if (OPTABcheck(opcode)) {
-        if ()
+    char *op1;
+    char *op2;
+    char *op3;
+    char *op4;
+    int type = typeCheck(opcode);
+    if (*opcode != ';') {
+      if (optabcheck(opcode)) {
+        if (opcode[strlen(opcode) - 2] == ';') {
+          opcode[strlen(opcode) - 2] = 0;
+          printf("Label = %s\n", opcode);
+          if (symtabContains(symtab, opcode)) {
+            if (type == 2) {
+              bmem = getKeyVal(symtab, opcode) | optab(opcode);
+            } else if (type == 3) {
+              bmem = getKeyVal(symtab, opcode) | optab(opcode);
+            }
+          } else {
+            bmem = optab(opcode);
+          }
+        } else {
+          bmem = optab(opcode);
+          instr |= getCond(opcode);
+          //fuck this for now, cba
+          //gotta implement it for each time of instruction ffs
+          //use helper method
+        }
+      } else if (opcode == "byte" || opcode = "word") {
+        bmem = optab(opcode);
       }
     }
 
@@ -91,9 +118,9 @@ int main(int argc, char **argv)
 
 }
 
+
 /*** Lookup Functions ***/
-int symtabContains(SymbolTable m, char *k)
-{
+int symtabContains(SymbolTable m, char *k) {
   for (int i = 0; i < m.size; i++) {
     if (m.symbols[i].name == k)
       return 1;
@@ -116,8 +143,7 @@ uint8_t getKeyVal(SymbolTable m, char *k) {
 //switch statement on char array
 //return the machine code
 
-int equals(char* a, char* b)
-{
+int equals(char *a, char *b) {
   int length = strlen(a);
   int length2 = strlen(b);
   if (length != length2) {
@@ -134,46 +160,45 @@ int equals(char* a, char* b)
 // the instruction format, available addressing modes and length information
 
 //OPTAB (operation code table)
-int OPTAB(char* a)
-{
-  if(equals(a, "add")) {
+int optab(char *a) {
+  if (equals(a, "add")) {
     return 0x800000;
-  } else  if(equals(a, "sub")){
+  } else if (equals(a, "sub")) {
     return 0x400000;
-  } else  if(equals(a, "rsb")){
+  } else if (equals(a, "rsb")) {
     return 0x600000;
-  } else  if(equals(a, "and")){
+  } else if (equals(a, "and")) {
     return 0x0;
-  } else  if(equals(a, "eor")){
+  } else if (equals(a, "eor")) {
     return 0x200000;
-  } else  if(equals(a, "orr")){
+  } else if (equals(a, "orr")) {
     return 0x1800000;
-  } else  if(equals(a, "mov")){
+  } else if (equals(a, "mov")) {
     return 0x1A00000;
-  } else  if(equals(a, "tst")){
+  } else if (equals(a, "tst")) {
     return 0x1000000;
-  } else  if(equals(a, "teq")){
+  } else if (equals(a, "teq")) {
     return 0x1200000;
-  } else  if(equals(a, "cmp")){
+  } else if (equals(a, "cmp")) {
     return 0x1400000;
-  } else  if(equals(a, "mul")){
+  } else if (equals(a, "mul")) {
     return 0xE0000090;
-  } else  if(equals(a, "mla")){
+  } else if (equals(a, "mla")) {
     return 0xE0200090;
-  } else  if(equals(a, "ldr")){
+  } else if (equals(a, "ldr")) {
     return 0x4100000;
-  } else  if(equals(a, "str")){
+  } else if (equals(a, "str")) {
     return 0x4000000;
-  } else  if(equals(a, "b")){
+  } else if (equals(a, "b")) {
     return 0xEA000000;
-  } else  if(equals(a, "lsl")){
+  } else if (equals(a, "lsl")) {
     return 0x1A00000;           //unsure
   }
   return -1;
 }
 
-int OPTABcheck(char* a) {
-  if(OPTAB(a) != -1) {
+int optabcheck(char *a) {
+  if (optab(a) != -1) {
     return 1;
   }
   return 0;
--- src/assemble.h
+++ src/assemble.h
@@ -29,3 +29,4 @@ int contains(char *string, char c);
 int symtabContains(SymbolTable m, char *k);
 void symtabAdd(SymbolTable m, Symbol e);
 uint8_t getKeyVal(SymbolTable m, char *k);
+int getCond(char *opcode);
