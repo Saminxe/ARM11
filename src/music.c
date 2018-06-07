7dbd578ad1a660b0f3e4e16602c902c86ae6d777
samuel <st5317@ic.ac.uk>
2018-06-07 14:55:55 +0100

    Update on how the struct is used, still needs tweaking

--- src/music.c
+++ src/music.c
@@ -12,79 +12,78 @@ typedef struct probability {
   double right3;
   double right4;
   double same;
-};
+}prob;
 
 enum notes{A1, B1, C1, D1, E1, F1, G1, A2, B2, C2, D2, E2, F2, G2, A3};
 
 void setProbability(enum notes note) {
-  struct probability *probabilityPtr, prob;
-  probabilityPtr = &prob;
-  probabilityPtr->same = 0.2;
+  struct probability prob;
+  prob.same = 0.2;
 
   if (note > 3 && note < 11 && note !=6 && note != 9) {  //notes outside this range could go out of bounds or is G1/C2
-    probabilityPtr->left1 = 0.025;
-    probabilityPtr->left2 = 0.2;
-    probabilityPtr->left3 = 0.025;
-    probabilityPtr->left4 = 0.15;
-    probabilityPtr->right1 = 0.025;
-    probabilityPtr->right2 = 0.2;
-    probabilityPtr->right3 = 0.025;
-    probabilityPtr->right4 = 0.15;
+    prob.left1 = 0.025;
+    prob.left2 = 0.2;
+    prob.left3 = 0.025;
+    prob.left4 = 0.15;
+    prob.right1 = 0.025;
+    prob.right2 = 0.2;
+    prob.right3 = 0.025;
+    prob.right4 = 0.15;
   } else if (note == 6) {                            //G1 is a special case in C Major
-    probabilityPtr->left1 = 0.025;
-    probabilityPtr->left2 = 0.2;
-    probabilityPtr->left3 = 0.025;
-    probabilityPtr->left4 = 0.15;
-    probabilityPtr->right1 = 0.025;
-    probabilityPtr->right2 = 0.1;
-    probabilityPtr->right3 = 0.25;
-    probabilityPtr->right4 = 0.025;
+    prob.left1 = 0.025;
+    prob.left2 = 0.2;
+    prob.left3 = 0.025;
+    prob.left4 = 0.15;
+    prob.right1 = 0.025;
+    prob.right2 = 0.1;
+    prob.right3 = 0.25;
+    prob.right4 = 0.025;
   } else if (note == 9) {                            //C2 is also special but in the reverse manner to G1
-    probabilityPtr->left1 = 0.025;
-    probabilityPtr->left2 = 0.1;
-    probabilityPtr->left3 = 0.25;
-    probabilityPtr->left4 = 0.025;
-    probabilityPtr->right1 = 0.025;
-    probabilityPtr->right2 = 0.2;
-    probabilityPtr->right3 = 0.025;
-    probabilityPtr->right4 = 0.15;
+    prob.left1 = 0.025;
+    prob.left2 = 0.1;
+    prob.left3 = 0.25;
+    prob.left4 = 0.025;
+    prob.right1 = 0.025;
+    prob.right2 = 0.2;
+    prob.right3 = 0.025;
+    prob.right4 = 0.15;
   }
   //I understand nested if statements would use less repeated code but it would also look disgusting
 
   switch(note) {
     case (A1):
-      probabilityPtr->left1 = probabilityPtr->left2 = probabilityPtr->left3 = probabilityPtr->left4 = 0;
-      probabilityPtr->right1 = 0.05;
-      probabilityPtr->right2 = 0.4;
-      probabilityPtr->right3 = 0.05;
-      probabilityPtr->right4 = 0.3;
+      prob.left1 = prob.left2 = prob.left3 = prob.left4 = 0;
+      prob.right1 = 0.05;
+      prob.right2 = 0.4;
+      prob.right3 = 0.05;
+      prob.right4 = 0.3;
       break;
     case (B1):
-      probabilityPtr->left2 = probabilityPtr->left3 = probabilityPtr->left4 = 0;
-      probabilityPtr->right1 = 0.05;
-      probabilityPtr->right2 = 0.375;
-      probabilityPtr->right3 = 0.05;
-      probabilityPtr->right4 = 0.275;
-      probabilityPtr->left1 = 0.05;
+      prob.left2 = prob.left3 = prob.left4 = 0;
+      prob.right1 = 0.05;
+      prob.right2 = 0.375;
+      prob.right3 = 0.05;
+      prob.right4 = 0.275;
+      prob.left1 = 0.05;
       break;
     case (C1):
-      probabilityPtr->left3 = probabilityPtr->left4 = 0;
-      probabilityPtr->right1 = 0.05;
-      probabilityPtr->right2 = 0.23;
-      probabilityPtr->right3 = 0.05;
-      probabilityPtr->right4 = 0.19;
-      probabilityPtr->left1 = 0.05;
-      probabilityPtr->left2 = 0.23;
+      prob.left3 = prob.left4 = 0;
+      prob.right1 = 0.05;
+      prob.right2 = 0.23;
+      prob.right3 = 0.05;
+      prob.right4 = 0.19;
+      prob.left1 = 0.05;
+      prob.left2 = 0.23;
       break;
     case (D1):
-      probabilityPtr->left4 = 0;
-      probabilityPtr->right1 = 0.05;
-      probabilityPtr->right2 = 0.21;
-      probabilityPtr->right3 = 0.05;
-      probabilityPtr->right4 = 0.16;
-      probabilityPtr->left1 = 0.05;
-      probabilityPtr->left2 = 0.21;
-      probabilityPtr->left3 = 0.05;
+      prob.left4 = 0;
+      prob.right1 = 0.05;
+      prob.right2 = 0.21;
+      prob.right3 = 0.05;
+      prob.right4 = 0.16;
+      prob.left1 = 0.05;
+      prob.left2 = 0.21;
+      prob.left3 = 0.05;
       break;
     case (E1):
       break;
@@ -101,37 +100,45 @@ void setProbability(enum notes note) {
     case (D2):
       break;
     case (E2):
-      probabilityPtr->right4 = 0;
-      probabilityPtr->left1 = 0.05;
-      probabilityPtr->left2 = 0.21;
-      probabilityPtr->left3 = 0.05;
-      probabilityPtr->left4 = 0.16;
-      probabilityPtr->right1 = 0.05;
-      probabilityPtr->right2 = 0.21;
-      probabilityPtr->right3 = 0.05;
+      prob.right4 = 0;
+      prob.left1 = 0.05;
+      prob.left2 = 0.21;
+      prob.left3 = 0.05;
+      prob.left4 = 0.16;
+      prob.right1 = 0.05;
+      prob.right2 = 0.21;
+      prob.right3 = 0.05;
       break;
     case (F2):
-      probabilityPtr->right3 = probabilityPtr->right4 = 0;
-      probabilityPtr->left1 = 0.05;
-      probabilityPtr->left2 = 0.23;
-      probabilityPtr->left3 = 0.05;
-      probabilityPtr->left4 = 0.19;
-      probabilityPtr->right1 = 0.05;
-      probabilityPtr->right2 =0.23;
+      prob.right3 = prob.right4 = 0;
+      prob.left1 = 0.05;
+      prob.left2 = 0.23;
+      prob.left3 = 0.05;
+      prob.left4 = 0.19;
+      prob.right1 = 0.05;
+      prob.right2 =0.23;
       break;
     case (G2):
-      probabilityPtr->right2 = probabilityPtr->right3 = probabilityPtr->right4 = 0;
-      probabilityPtr->left1 = 0.05;
-      probabilityPtr->left2 = 0.375;
-      probabilityPtr->left3 = 0.05;
-      probabilityPtr->left4 = 0.275;
-      probabilityPtr->right1 = 0.05;
+      prob.right2 = prob.right3 = prob.right4 = 0;
+      prob.left1 = 0.05;
+      prob.left2 = 0.375;
+      prob.left3 = 0.05;
+      prob.left4 = 0.275;
+      prob.right1 = 0.05;
     case (A3):
-      probabilityPtr->right1 = probabilityPtr->right2 = probabilityPtr->right3 = probabilityPtr->right4 = 0;
-      probabilityPtr->left1 = 0.05;
-      probabilityPtr->left2 = 0.4;
-      probabilityPtr->left3 = 0.05;
-      probabilityPtr->left4 = 0.3;
+      prob.right1 = prob.right2 = prob.right3 = prob.right4 = 0;
+      prob.left1 = 0.05;
+      prob.left2 = 0.4;
+      prob.left3 = 0.05;
+      prob.left4 = 0.3;
   }
 
+}
+
+//void shift(probability prob) {
+//
+//}
+
+int main() {
+  return 0;
 }
