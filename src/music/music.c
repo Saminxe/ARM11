56ce9c3676f3d6d4e81e247a5d80e1c8e108303f
samuel <st5317@ic.ac.uk>
2018-06-13 12:49:09 +0100

    we've peaked

--- src/music/music.c
+++ src/music/music.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#include <stdint.h>
 
 //This is for C Major
 
@@ -18,8 +19,10 @@ struct probability {
   int same;
 } prob;
 
-enum notes{A1, B1, C1, D1, E1, F1, G1, A2, B2, C2, D2, E2, F2, G2, A3};
+enum notes{A1 = 0x2D, B1 = 0x2F, C1 = 0x30, D1 = 0x32, E1 = 0x34, F1 = 0x35, G1 = 0x37, A2 = 0x39, B2 = 0x3B, C2 = 0x3C, D2 = 0x3E, E2 = 0x40, F2 = 0x42, G2 = 0x43, A3 = 0x45};
 
+
+//Sets the probabilities that are based off our personal musical experience
 void setProbability(enum notes note) {
   //probabilities are cumulative for our function to work
   prob.same = 100;
@@ -142,26 +145,27 @@ void setProbability(enum notes note) {
   }
 }
 
+//Randomly chooses a move based off of the probabilities
 int chooseNext() {
-  //randomly chooses a move based off of the probabilities
-  srand(time(NULL));
+
   int num = rand() % 101;
+  printf("%u\n", num);
   if (num > 0 && num <= prob.left1) {
-    return -1;
-  } else  if (num > prob.left1 && num <= prob.left2) {
     return -2;
-  } else  if (num > prob.left2 && num <= prob.left3) {
-    return -3;
-  } else  if (num > prob.left3 && num <= prob.left4) {
+  } else  if (num > prob.left1 && num <= prob.left2) {
     return -4;
+  } else  if (num > prob.left2 && num <= prob.left3) {
+    return -6;
+  } else  if (num > prob.left3 && num <= prob.left4) {
+    return -8;
   } else  if (num > prob.left4 && num <= prob.right1) {
-    return 1;
-  } else  if (num > prob.right1 && num <= prob.right2) {
     return 2;
-  } else  if (num > prob.right2 && num <= prob.right3) {
-    return 3;
-  } else  if (num > prob.right3 && num <= prob.right4) {
+  } else  if (num > prob.right1 && num <= prob.right2) {
     return 4;
+  } else  if (num > prob.right2 && num <= prob.right3) {
+    return 6;
+  } else  if (num > prob.right3 && num <= prob.right4) {
+    return 8;
   } else  if (num > prob.right4 && num <= prob.same) {
     return 0;
   }
@@ -169,101 +173,128 @@ int chooseNext() {
 
 }
 
-int shift(enum notes note) {
+//Applies the shifted vlaue to the notes
+uint8_t shift(enum notes note) {
   note += chooseNext();
   return note;
 }
-//applies the shifted vlaue to the notes
 
 
-int enumToInt(char *note) {
+//Converts the input String to it's numerical representation
+uint8_t enumToInt(char *note) {
   if (!strcmp(note, "A1")) {
-    return 0;
+    return 0x2D;
   } else if(!strcmp(note, "B1")) {
-    return 1;
+    return 0x2F;
   } else if(!strcmp(note, "C1")) {
-    return 2;
+    return 0x30;
   } else if(!strcmp(note, "D1")) {
-    return 3;
+    return 0x32;
   } else if(!strcmp(note, "E1")) {
-    return 4;
+    return 0x34;
   } else if(!strcmp(note, "F1")) {
-    return 5;
+    return 0x35;
   } else if(!strcmp(note, "G1")) {
-    return 6;
+    return 0x37;
   } else if(!strcmp(note, "A2")) {
-    return 7;
+    return 0x39;
   } else if(!strcmp(note, "B2")) {
-    return 8;
+    return 0x3B;
   } else if(!strcmp(note, "C2")) {
-    return 9;
+    return 0x3C;
   } else if(!strcmp(note, "D2")) {
-    return 10;
+    return 0x3E;
   } else if(!strcmp(note, "E2")) {
-    return 11;
+    return 0x40;
   } else if(!strcmp(note, "F2")) {
-    return 12;
+    return 0x42;
   } else if(!strcmp(note, "G2")) {
-    return 13;
+    return 0x43;
   } else if(!strcmp(note, "A3")) {
-    return 14;
+    return 0x45;
   }
   printf("Not a valid note you schmuk\n");
   exit(EXIT_FAILURE);
 }
 
-char *intToEnum(int num) {
-
-  switch (num) {
-    case (0): return "A1";
-    case (1): return "B1";
-    case (2): return "C1";
-    case (3): return "D1";
-    case (4): return "E1";
-    case (5): return "F1";
-    case (6): return "G1";
-    case (7): return "A2";
-    case (8): return "B2";
-    case (9): return "C2";
-    case (10): return "D2";
-    case (11): return "E2";
-    case (12): return "F2";
-    case (13): return "G2";
-    case (14): return "A3";
-    default: printf("Not a valid number you schmuk\n");
-  }
-  exit(EXIT_FAILURE);
-}
-
-int concatenate(int x, int y) {
-  int pow = 10;
-  while(y >= pow)
-    pow *= 10;
-  return x * pow + y;
-}
 
 int main(void) {
+  srand(time(NULL));
   FILE *f = fopen("notes.damn", "wb");
   if (f == NULL)
   {
     printf("Error opening file!\n");
     exit(1);
   }
+
+  //Here is where we create the DAMN header so that the files can be read and understood
+  char damn[] = {'D','A','M','N'};
+  uint32_t no_instr = 2;
+  uint8_t tempo = 0x78;
+  uint8_t fpb = 0x04;
+  uint16_t non = 0x0;
+  uint16_t attack = 0x5;
+  uint16_t decay = 0x5;
+  uint16_t sustain = 0x32;
+  uint16_t release = 0x5;
+  uint8_t waveshape = 0x0;
+  uint8_t waveshape2 = 0x1;
+  uint8_t waveshape3 = 0x3;
+  uint8_t oscillator_volume = 0x64;
+  uint16_t offset = 0x0;
+  uint32_t detune = 0x0;
+  uint64_t osc = 0x0;
+
+  fwrite(damn, 1, 4, f);
+  fwrite(&no_instr, 1, 4, f);
+  fwrite(&tempo, 1, 1, f);
+  fwrite(&fpb, 1, 1, f);
+  fwrite(&non, 1, 2, f);
+  fwrite(&non, 1, 2, f);
+  fwrite(&non, 1, 2, f);
+  fwrite(&attack, 1, 2, f);
+  fwrite(&decay, 1, 2, f);
+  fwrite(&sustain, 1, 2, f);
+  fwrite(&release, 1, 2, f);
+  fwrite(&waveshape, 1, 1, f);
+  fwrite(&oscillator_volume, 1, 1, f);
+  fwrite(&offset, 1, 2, f);
+  fwrite(&detune, 1, 4, f);
+  for (int i = 0; i < 8; i++) fwrite(&osc, 1, 4, f);
+  fwrite(&attack, 1, 2, f);
+  fwrite(&decay, 1, 2, f);
+  fwrite(&sustain, 1, 2, f);
+  fwrite(&release, 1, 2, f);
+  fwrite(&waveshape2, 1, 1, f);
+  fwrite(&oscillator_volume, 1, 1, f);
+  fwrite(&offset, 1, 2, f);
+  fwrite(&detune, 1, 4, f);
+  for (int i = 0; i < 8; i++) fwrite(&osc, 1, 4, f);
+  fwrite(&attack, 1, 2, f);
+  fwrite(&decay, 1, 2, f);
+  fwrite(&sustain, 1, 2, f);
+  fwrite(&release, 1, 2, f);
+  fwrite(&waveshape3, 1, 1, f);
+  fwrite(&oscillator_volume, 1, 1, f);
+  fwrite(&offset, 1, 2, f);
+  fwrite(&detune, 1, 4, f);
+  for (int i = 0; i < 8; i++) fwrite(&osc, 1, 4, f);
+
   srand(time(NULL));
   char *note = malloc(sizeof(char*));
   scanf("%s", note);
-  int num = rand() % 201;
-  //int muse = enumToInt(note);
-  int muse[200];
-  int temp = enumToInt(note);
-  for (int i = 0; i < num; ++i) {
-    muse[i] = temp;
+  uint8_t *muse;
+  uint8_t temp = enumToInt(note);
+  uint8_t velocity = 0x28;
+  for (int i = 0; i < 6000; ++i) {
+    muse = &temp;
+    fwrite(muse, 1, 1, f);
+    fwrite(&velocity, 1, 1, f);
+    fwrite(&non, 1, 2, f);
     setProbability(temp);
-    temp = shift(temp);
-    //muse = concatenate(muse, temp);
+    temp = shift(temp) % 74 + 23;
   }
-  fwrite(muse, sizeof(int), 200, f);
-  //fprintf(f, "%i", muse);
+  free(note);
   fclose(f);
   return 0;
 }
